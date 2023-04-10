8965dd18f02a56c6c430693932f48de940a2c8ed
31415wataru <63274202+31415wataru@users.noreply.github.com>
2023-04-10 14:33:53 +0900

    授業内

---
+++ 1-1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(void){
+
+    printf("Hello!! \n My name is Biden\n");
+    printf("8+21 is %d\n",8+21);
+    printf("The sum of 8 and 21 in hexadecimal is %x",8+21);
+    }
---
+++ 1-2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void){
+    float teihen,takasa,menseki;
+
+    printf("Enter base lenght\n");
+    scanf("%f\n",&teihen);
+    printf("Enter height\n");
+    scanf("%f\n",&takasa);
+
+   menseki = teihen*takasa*0.5;
+   printf("A triangle with base %f and height %f has area of %f",teihen,takasa,menseki);
+}
