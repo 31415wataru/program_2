98c05f0339f8449c8f895aa30524b1e5e640d714
31415wataru <63274202+31415wataru@users.noreply.github.com>
2023-06-25 09:53:05 +0900

    課題2を追加

---
+++ 11-課題2_.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define smax 100
+struct element{
+ char data;
+ struct element *next;
+};
+struct stack{
+ char box[smax];
+ int top;
+};
+#include "code2_3.h"
+void printall( struct element *p){
+  while (p) {
+  printf("%c\n", p->data);
+  p = p->next;
+  }
+}
+void printall_reverse( struct element *p){
+     if(p->next == NULL) {
+        putchar('\n');
+    }else {
+        p = p->next;
+        putchar(p->data);
+        putchar('\n');
+        printall_reverse(p);
+        putchar('\n');
+        putchar(p->data);
+    }
+}
+int main(void){
+
+ struct element *list;
+
+ list = create();
+ insert(list, 1, 'A');
+ insert(list, 2, 'B');
+ insert(list, 2, 'a');
+ insert(list, 4, 'b');
+ //printall(list);
+ printf("\n");
+ printall_reverse(list);
+ return 0;
+}
